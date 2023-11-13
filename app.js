1ced9b20e6b945849cb7e775cc24c7b8312beef0
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-11-12 22:23:24 -0300

    clase de servidor dinamico

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ const port = 3000;
 
 const app = express();
 
+// yo este app lo puedo llamar cuantas veces quiera para traerme un ruta o mostrar un mensajito
 app.get("/", (req, res) => {
   res.send("Hola Mundo!");
 });
 
+/* aca yo estoy usando los dos modulos de express, el fs y el path , los dos son necesarios para traerme un archivo y cada uno tiene sus metodos dentro de si mismo
+por ejemplo  path.join ( me traigo un archivo que esta en x posicion de mis documentos)
+
+por ejemplo fs.readFile ( le indico que archivo me va a leer y en que idioma esta y ademas le pongo el error y la data)
+
+analizar aca ojo gloria y hacerlo igual con otro archivo json que puedo crear yo sola 
+
+
+*/
 app.get("/personajes/:id", (req, res) => {
   const dibujo = path.join(__dirname, "/src/data/personajes.json");
 
